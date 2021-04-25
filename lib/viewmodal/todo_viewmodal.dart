51a1f525ef7708371c5d12a5ab13199b8cf6f1ef
vietnguyenhoangw <viet.nguyen@itbeesolutions.vn>
2021-04-25 17:23:27 +0700

    :penguin: Add toast

--- lib/viewmodal/todo_viewmodal.dart
+++ lib/viewmodal/todo_viewmodal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:todolist_with_provider/modal/todo.dart';
 import 'package:todolist_with_provider/modal/todo_list.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class TodoViewModal extends ChangeNotifier {
   TodoListModal todoListModal = new TodoListModal();
@@ -12,15 +14,26 @@ class TodoViewModal extends ChangeNotifier {
     ScrollController scrollController,
     BuildContext context,
   ) {
-    TodoModal newTodo = new TodoModal();
-    newTodo.set_taskName(taskInputController.text);
-    todoListModal.add_todo_item(newTodo);
-    getListTodo();
-    taskInputController.text = "";
-    scrollController?.animateTo(scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
-    FocusScope.of(context).requestFocus(FocusNode());
-    notifyListeners();
+    if (taskInputController.text.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "Task title is empty",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black38,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    } else {
+      TodoModal newTodo = new TodoModal();
+      newTodo.set_taskName(taskInputController.text);
+      todoListModal.add_todo_item(newTodo);
+      getListTodo();
+      taskInputController.text = "";
+      scrollController?.animateTo(scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
+      FocusScope.of(context).requestFocus(FocusNode());
+      notifyListeners();
+    }
   }
 
   void getListTodo() {
